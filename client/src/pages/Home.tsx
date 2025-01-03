96413b7b9eec35b3660f48c1a94468c9c7a661e1
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-02 16:07:04 -0800

    Homeに新たなコンポーネントを追加

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,25 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import MonthlySummary from '../components/MonthlySummary'
+import Calendar from '../components/Calendar'
+import TransactionMenu from '../components/TransactionMenu'
+import TransactionForm from '../components/TransactionForm'
 
 const Home = () => {
   return (
-    <div>
-      h
-    </div>
+    <Box sx={{display: 'flex'}}>
+      {/* 左側コンテンツ */}
+      <Box sx={{flexGrow: 1, bgcolor: 'pink'}}>
+        <MonthlySummary />
+        <Calendar />
+      </Box>
+
+      {/* 右側コンテンツ */}
+      <Box>
+        <TransactionMenu />
+        <TransactionForm />
+      </Box>
+    </Box>
   )
 }
 
